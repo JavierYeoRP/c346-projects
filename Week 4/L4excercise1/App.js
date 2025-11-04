7b94d61e067af4dbe820823766f41949d7e19705
Javier Yeo <23006889@myrp.edu.sg>
2025-11-04 11:58:41 +0800

    Lesson 4 Excercise #1

---
+++ Week 4/L4excercise1/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Text } from 'react-native';
+
+const App = () => (
+  <>
+    <Text style={{ fontSize: 24 }}>RP Values</Text>
+    <Text style={{ color: 'green' }}>Excellence</Text>
+    <Text style={{ backgroundColor: 'yellow' }}>Customer-Centric</Text>
+    <Text style={{ fontStyle: 'italic' }}>Integrity</Text>
+    <Text style={{ textAlign: 'center', width: '100%' }}>Teamwork</Text>
+    <Text style={{ backgroundColor: 'black', color: 'white' }}>Enterprising</Text>
+  </>
+);
+
+export default App;
